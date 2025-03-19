cc5f424d7a442e9b2b49e827829e9e4840bd7b1b
IbrohimSaypillayev <sarahconoruzb@gmail.com>
2025-03-19 11:34:23 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,9 @@
+let ismingiz = prompt("ismingizni kiriting 😀")
+let harf = prompt('birorta harif kiriting 😃')
+
+
+if(ismingiz.includes(harf)){
+    alert(`ichida ${harf} harfi bor`)
+}else{
+    alert(`ichida ${harf}harfi yoq`)
+}
